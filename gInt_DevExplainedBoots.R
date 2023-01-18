09c8ae74d722fb8d900162fccb45478d7304626e
adpines <apines@stanford.edu>
2023-01-18 11:23:49 -0800

    commit for int and ext dev explained for proofreading

--- gInt_DevExplainedBoots.R
+++ gInt_DevExplainedBoots.R
@@ -5,15 +5,29 @@ masterdf=readRDS('/oak/stanford/groups/leanew1/users/apines/data/gp/mixedEfDf.rd
 devExplained_full=rep(0,1000)
 devExplained_NoCbcl61=rep(0,1000)
 devExplained_NoTot=rep(0,1000)
+i_devExplained_full=rep(0,1000)
+i_devExplained_NoCbcl61=rep(0,1000)
+i_devExplained_NoG=rep(0,1000)
 # garner num subjs for bootstrapping
-numSubjs=dim(masterdf)[1]
+subjs=unique(masterdf$subjectkey)
+numSubjs=length(subjs)
+# cut df to just variables of interest to speed stuff up
+masterdf=masterdf[,c('cbcl_scr_syn_internal_r','g','subjectkey','interview_age','cbcl_q61_p')]
 # loop over manual bootstrap
 for (b in 1:10000){
 	print(b)
-	BootInd<-sample(seq(1,numSubjs),numSubjs,replace=T)
+	# get subjects to include in this bootstrap
+	BootSubjs=sample(subjs,numSubjs,replace=T)
+	### inefficient but interpretable loop
+	# Create an empty dataframe to store the resampled observations
+	resampled_df <- data.frame()
+	for (j in 1:length(BootSubjs)){
+		subject_obs <- masterdf[masterdf$subjectkey == BootSubjs[j], ]
+		resampled_df <- rbind(resampled_df, subject_obs)
+	}
 	# bootstrap sample
-	bootSamp=masterdf[BootInd,]
-	#### gpAge_independent splines
+	bootSamp=resampled_df
+	# full model
 	gpAge_full<-bam(g~s(cbcl_scr_syn_internal_r)+s(interview_age)+cbcl_q61_p,data=bootSamp)
 	devExplained_full[b]<-summary(gpAge_full)$dev.expl
 	# fit version with cbcl 61
@@ -22,9 +36,17 @@ for (b in 1:10000){
 	# fit version with just cbcl61
 	gpAge_noTotProbs<-bam(g~cbcl_q61_p+s(interview_age),data=bootSamp)
 	devExplained_NoTot[b]<-summary(gpAge_noTotProbs)$dev.expl
+	### version with symptom count as response variable
+	pgAge_full<-bam(cbcl_scr_syn_internal_r~s(g)+s(interview_age)+cbcl_q61_p,data=bootSamp,family=nb())
+	i_devExplained_full[b]<-summary(pgAge_full)$dev.expl
+	# fit version with cbcl 61
+	pgAge_nocbcl61<-bam(cbcl_scr_syn_internal_r~s(g)+s(interview_age),data=bootSamp,family=nb())
+	i_devExplained_NoCbcl61[b]<-summary(pgAge_nocbcl61)$dev.expl
+	# fit version with just cbcl61
+	pgAge_noG<-bam(cbcl_scr_syn_internal_r~cbcl_q61_p+s(interview_age),data=bootSamp,family=nb())
+	i_devExplained_NoG[b]<-summary(pgAge_noG)$dev.expl
 }
-
 # saveout df of dev explained for plotting
-outdf=data.frame(devExplained_full,devExplained_NoCbcl61,devExplained_NoTot)
+outdf=data.frame(devExplained_full,devExplained_NoCbcl61,devExplained_NoTot,i_devExplained_full,i_devExplained_NoCbcl61,i_devExplained_NoG)
 saveRDS(outdf,'/oak/stanford/groups/leanew1/users/apines/data/gp/DevExplainedInt.rds')
 
