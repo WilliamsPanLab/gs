c5c9c374b87a4d7fb02e746981abae0d91eb8af5
adpines <apines@stanford.edu>
2023-03-19 16:38:58 -0700

    minor, resampling fs5 consensus to fslr

--- FunctionalImages/Networks/Resample_fs5mat_to_dscalar.m
+++ FunctionalImages/Networks/Resample_fs5mat_to_dscalar.m
@@ -11,12 +11,11 @@ V=V{1};
 %%% use snr mask to translate back to 10242 per hemisphere
 % load in mask (SNR Mask)
 surfML = '/oak/stanford/groups/leanew1/users/apines/data/gp/lh.Mask_SNR.label';
-surfML = '/oak/stanford/groups/leanew1/users/apines/data/gp/lh.Mask_SNR.label'
 mwIndVec_l = read_medial_wall_label(surfML);
 % get difference for valid vertices
 index_l=setdiff([1:10242],mwIndVec_l);
 % right hemisphere
-surfMR = '/oak/stanford/groups/leanew1/users/apines/maps/rh.cortex_SNR.label';
+surfMR = '/oak/stanford/groups/leanew1/users/apines/data/gp/rh.Mask_SNR.label';
 mwIndVec_r = read_medial_wall_label(surfMR);
 index_r=setdiff([1:10242],mwIndVec_r);
 
@@ -29,35 +28,37 @@ k=18
 % now for each network, plop mat data into funcgii and save a copy for merging back together at the end of this script (to then be plopped into a 32k-equiv initMat)	
 for n=1:k
 	% put current network into LeftHemi and RightHemi
-	% insert that non-medial wall data from the consensus partition
-	
-	%%%LeftHemi(mwIndVec_l)=V(1:length(mwIndVec_l),n);
-	%%%RightHemi(mwIndVec_r)=V(length(mwIndVec_l)+1:(length(mwIndVec_r)+length(mwIndVec_l)),n);
-
-	% use left and right hemi PG func.giis as templates to save parcellation over
-	LH=gifti('/oak/stanford/groups/leanew1/users/apines/maps/hcp.gradients_10k_L.dscalar.func.gii')
-	RH=gifti('/oak/stanford/groups/leanew1/users/apines/maps/hcp.gradients_10k_R.dscalar.func.gii')
 	% zero-out current vector
 	LH.cdata(:,n)=0
+	RH.cdata(:,n)=0
 	% replace with indices from V
 	LH.cdata(index_l,n)=V(1:length(index_l),n);
-
-		% ADOPT FASHION ABOVE ^
-
-	RH.cdata(mwIndVec_r,n)=V(length(mwIndVec_l)+1:end),n);
-
+	RH.cdata(index_r,n)=V(length(index_l)+1:end,n);
 	% set filepath
 	LHfp=['/oak/stanford/groups/leanew1/users/apines/maps/' num2str(n) '_groL.func.gii'];
 	RHfp=['/oak/stanford/groups/leanew1/users/apines/maps/' num2str(n) '_groR.func.gii'];
 	% saveout
 	save(LH, LHfp);
-	save(RH, RHfp);
-
-	
+	save(RH, RHfp);	
 	% sys command for resampling
+	commandL=['wb_command -metric-resample ' LHfp ' /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fsaverage5_std_sphere.L.10k_fsavg_L.surf.gii /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fs_LR-deformed_to-fsaverage.L.sphere.32k_fs_LR.surf.gii ADAP_BARY_AREA /oak/stanford/groups/leanew1/users/apines/conversion_tests/resamp_32k_network' num2str(n) '_L.32k_fs_LR.func.gii -area-metrics /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fsaverage5.L.midthickness_va_avg.10k_fsavg_L.shape.gii /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fs_LR.L.midthickness_va_avg.32k_fs_LR.shape.gii'];
+	commandR=['wb_command -metric-resample ' RHfp ' /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fsaverage5_std_sphere.R.10k_fsavg_R.surf.gii /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fs_LR-deformed_to-fsaverage.R.sphere.32k_fs_LR.surf.gii ADAP_BARY_AREA /oak/stanford/groups/leanew1/users/apines/conversion_tests/resamp_32k_network' num2str(n) '_R.32k_fs_LR.func.gii -area-metrics /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fsaverage5.R.midthickness_va_avg.10k_fsavg_R.shape.gii /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fs_LR.R.midthickness_va_avg.32k_fs_LR.shape.gii'];
+	system(commandL)
+	system(commandR)
+	% downsampled and upsampled func.gii's should be done for this network
+end
+% next step is to convert upsampled into init.mat again, next loop
+% initialize initmat
+for n=1:k
+	% load upsampled gifti
+	% populate initmat with each upsampled gifti (shouldn't need SNR mask)
+end
+% saveout initmat
 
 
-
+%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%
 
 
 outputfile=['/cbica/projects/abcdfnets/results/SingleParcel_1by1/' subj '/' subj '_Parcel.dscalar.nii'];
