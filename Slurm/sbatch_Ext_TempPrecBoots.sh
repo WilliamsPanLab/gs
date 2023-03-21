ebce97797f1ee7fc8a3333a90ed2c5f082c1dfcf
adpines <apines@stanford.edu>
2023-03-20 18:28:57 -0700

    ive updates, dropping g nomenclature

---
+++ Slurm/sbatch_Ext_TempPrecBoots.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --job-name=ExtTmpPrec
+#SBATCH --time=7-00:00:00
+#SBATCH -p leanew1  # Queue names you can submit to
+#SBATCH -n 1
+#SBATCH --mem=110G
+# Outputs ----------------------------------
+#SBATCH --mail-user=apines@stanford.edu
+#SBATCH --mail-type=ALL
+# ------------------------------------------
+module load R/4.1
+
+Rscript Ext_TempPrecBoots.R
+
+# echo intermission
+
+# Rscript Extg_TemPrecBoots.R
