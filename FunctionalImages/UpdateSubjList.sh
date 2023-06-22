4332197c546ab2fcba97a1324e47ebaf23b86bcc
adpines <apines@stanford.edu>
2023-06-22 08:25:40 -0700

    Delete FunctionalImages directory

--- FunctionalImages/UpdateSubjList.sh
+++
@@ -1,9 +0,0 @@
-# set list to copy to new list
-cp /oak/stanford/groups/leanew1/users/apines/data/gp/DL_list.txt /oak/stanford/groups/leanew1/users/apines/data/gp/DL_list2.txt
-
-for subject in $(cat /oak/stanford/groups/leanew1/users/apines/data/gp/DL_list2.txt); do
-    if [ -f "/oak/stanford/groups/leanew1/users/apines/data/gp/PropFeats/$subject/Prop_Feats.csv" ]; then
-        sed -i "/$subject/d" /oak/stanford/groups/leanew1/users/apines/data/gp/DL_list2.txt
-    fi
-done
-
--- FunctionalImages/VecNormalize.m
+++
@@ -1,8 +0,0 @@
-function V = VecNormalize(V)
-%NORMALIZE Normalizes row vectors.
-%
-%   V = normalise(V) takes a n-by-3 matrix V and returns normalises row
-%   vectors.
-len = sqrt(sum(V.^2, 2));
-V = bsxfun(@rdivide, V, len);
-end
