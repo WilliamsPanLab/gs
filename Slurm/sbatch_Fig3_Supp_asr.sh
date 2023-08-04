39f764bd9af79da0d1bb38811bab34e5102f9443
adpines <apines@stanford.edu>
2023-08-04 16:58:26 -0700

    fix for obsucre supp. analysis

---
+++ Slurm/sbatch_Fig3_Supp_asr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=Fig3_asr
+#SBATCH --time=4-00:00:00
+#SBATCH -p leanew1  # Queue names you can submit to
+#SBATCH -n 1
+#SBATCH --mem=70G
+# Outputs ----------------------------------
+#SBATCH --mail-user=apines@stanford.edu
+#SBATCH --mail-type=ALL
+# ------------------------------------------
+module load R/4.1
+
+Rscript Fig3_Boots_SuppASRSubscales.R
