23ce68e706de267add403f1d3ba13e9c6a9e904f
adpines <pines16@gmail.com>
2023-11-23 06:48:46 -0800

    subclin/clin boots

---
+++ Slurm/Fig1_Boots_SCOe.R
@@ -0,0 +1,53 @@
+library(mgcv)
+library(pammtools)
+library(dplyr)
+library(data.table)
+library(stringr)
+library(ggplot2)
+
+# load in data
+masterdf=readRDS('/oak/stanford/groups/leanew1/users/apines/data/gp/gp_SCOe_masterdf.rds')
+# FYI
+print('dimensions of dataframe')
+dim(masterdf)
+
+# garner num subjs for bootstrapping
+subjs=unique(masterdf$subjectkey)
+numSubjs=length(subjs)
+# cut df to just variables of interest to speed stuff up # add cbcl subscales
+masterdf=masterdf[,c('cbcl_scr_syn_totprob_r','cbcl_scr_syn_internal_r','cbcl_scr_syn_external_r','cbcl_scr_syn_somatic_r','cbcl_scr_syn_anxdep_r','cbcl_scr_syn_thought_r','cbcl_scr_syn_withdep_r','cbcl_scr_syn_social_r','cbcl_scr_syn_attention_r','cbcl_scr_syn_rulebreak_r','cbcl_scr_syn_aggressive_r','g','subjectkey','interview_age')]
+# get length of df for later
+lenDF=dim(masterdf)[1]
+# convert all cbcl scores to numeric
+masterdf$cbcl_scr_syn_external_r=as.numeric(masterdf$cbcl_scr_syn_external_r)
+# initialize output betas
+pBeta=rep(0,1000)
+# loop over manual bootstrap
+for (b in 1:1000){
+	print(b)
+	# get subjects to include in this bootstrap
+	BootSubjs=sample(subjs,numSubjs,replace=T)
+	### inefficient but interpretable loop
+	# Create an empty dataframe to store the resampled observations
+	bootSamp <- data.frame()
+	for (j in 1:length(BootSubjs)){
+		subject_obs <- masterdf[masterdf$subjectkey == BootSubjs[j], ]
+		bootSamp <- rbind(bootSamp, subject_obs)
+	}
+	##############################
+	# get max for this iteration
+	bpmax=max(bootSamp$cbcl_scr_syn_external_r)
+	######## II PREDICT VARIABLE OF INTEREST WITH FIT SPLINE
+	#### g as response variable
+	pgAge<-lm(g~cbcl_scr_syn_external_r+interview_age,data=bootSamp)
+	# print out max of unconverted versions to anchor em later
+	pBeta[b]=summary(pgAge)$coefficients[2,3]
+}
+# SAVEOUT
+# save out version with all cbcl factors
+outdf=data.frame(pBeta)
+saveRDS(outdf,'/oak/stanford/groups/leanew1/users/apines/data/gp/gp_SCOe_Boots.rds')
+
+
+print('done with g~p fit bootstrapping!')
+
